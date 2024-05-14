769116cdfdb00260cb4988949daaabd17d636fa1
user_miri <wnslaqkrdptj@gmail.com>
2024-05-14 15:49:32 +0900

    first complete

--- javascript/style.js
+++ javascript/style.js
@@ -16,15 +16,37 @@ function calculateAge() {
   var resultSpan = document.getElementById('result');
   resultSpan.textContent = "(만"+age+"세)";
 
-  function calculateExperienceForAllDates() {
+function calculateExperienceForAllDates() {
     // 모든 date id를 가진 요소를 선택
     var dateElements = document.querySelectorAll('[id^="date"]');
-    /*var totalExperience = { years: 0, months: 0 };*/
+    var experienceElements = document.querySelectorAll('[id^="experience"]');
+    var totalExperience = { years: 0, months: 0 };
+
+    for(var i =0;i<dateElements.length;i++){
+        dateElements[i].style.color="gray";
+        dateElements[i].style.size="2px";
+    }
+
+    for(var i=0;i<experienceElements.length;i++){
+        experienceElements[i].style.color="darkred";
+        experienceElements[i].style.size="2px";
+        experienceElements[i].style.fontStyle="italic";
+    }
 
     // 각 요소에 대해 경력 계산
     dateElements.forEach(function(dateElement) {
+        var experienceElements = dateElement.nextElementSibling; // 경력 텍스트를 표시할 요소 가져오기
         var dates = dateElement.textContent.split(' ~ ');
         var experience = calculateExperience(dates[0], dates[1]);
+        if(experience.years){
+            if(experience.months)
+                experienceElements.textContent = experience.years+"년 "+experience.months+"개월";
+            else
+                experienceElements.textContent = experience.years+"년 ";
+        }
+        else
+            experienceElements.textContent = experience.months+"개월";
+
         totalExperience.years += experience.years;
         totalExperience.months += experience.months;
 
@@ -39,6 +61,11 @@ function calculateAge() {
 }
 
 function calculateExperience(startDate, endDate) {
+    if(endDate === "재직중"){   /*js에서는 ==이 아니라 ===, _T는 안 쓴다. */
+        var today = new Date();
+        endDate = today;
+    }
+
     var start = new Date(startDate);
     var end = new Date(endDate);
     var yearDiff = end.getFullYear() - start.getFullYear();
@@ -51,4 +78,15 @@ function calculateExperience(startDate, endDate) {
 }
 
 var totalExperience = calculateExperienceForAllDates();
-console.log("전체 경력:", totalExperience.years, "년", totalExperience.months, "개월");
+var total_experienceSpan = document.getElementById('total_experience');
+
+if(totalExperience.years){
+    if(totalExperience.months)
+        total_experienceSpan.textContent="총 경력 : "+totalExperience.years+"년"+totalExperience.months+"개월";
+    else
+        total_experienceSpan.textContent="총 경력 : "+totalExperience.years+"년";
+}
+else
+    total_experienceSpan.textContent="총 경력 : "+totalExperience.months+"개월";
+
+/*console.log("전체 경력:", totalExperience.years, "년", totalExperience.months, "개월");*/
